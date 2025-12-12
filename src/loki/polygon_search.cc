bfa511ebb8679b44a228831a41929d0be1c23d93
Molnár Kristóf <kristof.molnar@envirosense.hu>
2025-12-12 20:01:38 +0100

    Initial commit

---
+++ src/loki/polygon_search.cc
@@ -0,0 +1,230 @@
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/register/point.hpp>
+#include <boost/geometry/geometries/register/ring.hpp>
+
+#include <valhalla/baldr/json.h>
+#include <valhalla/loki/polygon_search.h>
+#include <valhalla/midgard/constants.h>
+#include <valhalla/midgard/logging.h>
+#include <valhalla/midgard/pointll.h>
+#include <valhalla/midgard/util.h>
+#include <valhalla/worker.h>
+
+namespace bg = boost::geometry;
+namespace vm = valhalla::midgard;
+namespace vb = valhalla::baldr;
+namespace vl = valhalla::loki;
+
+BOOST_GEOMETRY_REGISTER_POINT_2D(vm::PointLL, double, bg::cs::geographic<bg::degree>, first, second)
+BOOST_GEOMETRY_REGISTER_RING(std::vector<vm::PointLL>)
+
+namespace {
+// register a few boost.geometry types
+using line_bg_t = bg::model::linestring<vm::PointLL>;
+using ring_bg_t = std::vector<vm::PointLL>;
+using namespace vb::json;
+
+// map of tile for map of bin ids & their ring ids
+// TODO: simplify the logic behind this a little
+using bins_collector =
+    std::unordered_map<uint32_t, std::unordered_map<unsigned short, std::vector<size_t>>>;
+
+static const auto Haversine = [] {
+  return bg::strategy::distance::haversine<float>(vm::kRadEarthMeters);
+};
+
+void correct_ring(ring_bg_t& ring) {
+  // close open rings
+  bool is_open =
+      (ring.begin()->lat() != ring.rbegin()->lat() || ring.begin()->lng() != ring.rbegin()->lng());
+  if (!ring.empty() && is_open) {
+    ring.push_back(ring[0]);
+  }
+
+  // reverse ring if counter-clockwise
+  if (vm::polygon_area(ring) > 0) {
+    std::reverse(ring.begin(), ring.end());
+  }
+}
+
+ring_bg_t PBFToRing(const valhalla::Ring& ring_pbf) {
+  ring_bg_t new_ring;
+  for (const auto& coord : ring_pbf.coords()) {
+    new_ring.push_back({coord.lng(), coord.lat()});
+  }
+  correct_ring(new_ring);
+  return new_ring;
+}
+
+#ifdef LOGGING_LEVEL_TRACE
+// serializes an edge to geojson
+std::string to_geojson(const std::unordered_set<vb::GraphId>& edge_ids, vb::GraphReader& reader) {
+  if (edge_ids.empty()) {
+    return "None found";
+  }
+  auto features = array({});
+  for (const auto& edge_id : edge_ids) {
+    auto tile = reader.GetGraphTile(edge_id);
+    auto edge = tile->directededge(edge_id);
+    auto shape = tile->edgeinfo(edge).shape();
+    if (!edge->forward()) {
+      std::reverse(shape.begin(), shape.end());
+    }
+
+    auto coords = array({});
+    for (const auto& p : shape) {
+      coords->emplace_back(array({fixed_t{p.lng(), 6}, fixed_t{p.lat(), 6}}));
+    }
+    features->emplace_back(
+        map({{"type", std::string("Feature")},
+             {"properties",
+              map({{"shortcut", edge->is_shortcut() ? std::string("True") : std::string("False")},
+                   {"edge_id", edge_id.value}})},
+             {"geometry", map({{"type", std::string("LineString")}, {"coordinates", coords}})}}));
+  }
+
+  auto collection =
+      vb::json::map({{"type", std::string("FeatureCollection")}, {"features", features}});
+
+  std::stringstream ss;
+  ss << *collection;
+
+  return ss.str();
+}
+#endif // LOGGING_LEVEL_TRACE
+} // namespace
+
+namespace valhalla {
+namespace loki {
+
+std::unordered_set<vb::GraphId> edges_in_rings(const Ring& ring_pbf,
+                                               baldr::GraphReader& reader,
+                                               const std::shared_ptr<sif::DynamicCost>& costing,
+                                               float max_length,
+                                               const SearchStrategy strategy) {
+  google::protobuf::RepeatedPtrField<valhalla::Ring> rings;
+  rings.Add()->CopyFrom(ring_pbf);
+
+  return edges_in_rings(rings, reader, costing, max_length, strategy);
+}
+
+std::unordered_set<vb::GraphId>
+edges_in_rings(const google::protobuf::RepeatedPtrField<valhalla::Ring>& rings_pbf,
+               baldr::GraphReader& reader,
+               const std::shared_ptr<sif::DynamicCost>& costing,
+               float max_length,
+               const SearchStrategy strategy) {
+  // protect for bogus input
+  if (rings_pbf.empty() || rings_pbf.Get(0).coords().empty() ||
+      !rings_pbf.Get(0).coords()[0].has_lat_case() || !rings_pbf.Get(0).coords()[0].has_lng_case()) {
+    return {};
+  }
+
+  // convert to bg object and check length restriction
+  double rings_length = 0;
+  std::vector<ring_bg_t> rings_bg;
+  for (const auto& ring_pbf : rings_pbf) {
+    rings_bg.push_back(PBFToRing(ring_pbf));
+    const ring_bg_t ring_bg = rings_bg.back();
+    rings_length += bg::perimeter(ring_bg, Haversine());
+  }
+  if (rings_length > max_length) {
+    if (strategy == SearchStrategy::AVOID) {
+      throw valhalla_exception_t(167, std::to_string(static_cast<size_t>(max_length)) + " meters");
+    } else if (strategy == SearchStrategy::CHINESE) {
+      throw valhalla_exception_t(173, std::to_string(static_cast<size_t>(max_length)) + " meters");
+    } else {
+      throw valhalla_exception_t(107);
+    }
+  }
+
+  // Get the lowest level and tiles
+  const auto tiles = vb::TileHierarchy::levels().back().tiles;
+  const auto bin_level = vb::TileHierarchy::levels().back().level;
+
+  // keep track which tile's bins intersect which rings
+  bins_collector bins_intersected;
+  std::unordered_set<vb::GraphId> result_edge_ids;
+
+  // first pull out all *unique* bins which intersect the rings
+  for (size_t ring_idx = 0; ring_idx < rings_bg.size(); ring_idx++) {
+    auto ring = rings_bg[ring_idx];
+    auto line_intersected = tiles.Intersect(ring);
+    for (const auto& tb : line_intersected) {
+      for (const auto& b : tb.second) {
+        bins_intersected[static_cast<uint32_t>(tb.first)][b].push_back(ring_idx);
+      }
+    }
+  }
+  for (const auto& intersection : bins_intersected) {
+    auto tile = reader.GetGraphTile({intersection.first, bin_level, 0});
+    if (!tile) {
+      continue;
+    }
+    for (const auto& bin : intersection.second) {
+      // tile will be mutated most likely in the loop
+      reader.GetGraphTile({intersection.first, bin_level, 0}, tile);
+      for (const auto& edge_id : tile->GetBin(bin.first)) {
+        if (result_edge_ids.count(edge_id) != 0) {
+          continue;
+        }
+        // TODO: optimize the tile switching by enqueuing edges
+        // from other levels & tiles and process them after this big loop
+        if (edge_id.Tile_Base() != tile->header()->graphid().Tile_Base() &&
+            !reader.GetGraphTile(edge_id, tile)) {
+          continue;
+        }
+        const auto edge = tile->directededge(edge_id);
+        auto opp_tile = tile;
+        const baldr::DirectedEdge* opp_edge = nullptr;
+        baldr::GraphId opp_id;
+        bool edge_allowed = false;
+        bool opp_allowed = false;
+
+        // bail if we wouldnt be allowed on this edge anyway (or its opposing)
+        if (!(edge_allowed = costing->Allowed(edge, tile)) &&
+            (!(opp_id = reader.GetOpposingEdgeId(edge_id, opp_edge, opp_tile)).Is_Valid() ||
+             !(opp_allowed = costing->Allowed(opp_edge, opp_tile)))) {
+          continue;
+        }
+        // TODO: some logic to set percent_along for origin/destination edges
+        // careful: polygon can intersect a single edge multiple times
+        auto edge_info = tile->edgeinfo(edge);
+        bool predicate = false;
+        for (const auto& ring_loc : bin.second) {
+          opp_id = opp_id.Is_Valid() ? opp_id : reader.GetOpposingEdgeId(edge_id, opp_edge, opp_tile);
+          if (strategy == SearchStrategy::AVOID) {
+            predicate = bg::intersects(rings_bg[ring_loc],
+                                       line_bg_t(edge_info.shape().begin(), edge_info.shape().end()));
+            // for avoids, at this point we want to add both regardless if one of them is not allowed
+            opp_allowed = edge_allowed = true;
+          } else if (strategy == SearchStrategy::CHINESE) {
+            predicate = bg::within(line_bg_t(edge_info.shape().begin(), edge_info.shape().end()),
+                                   rings_bg[ring_loc]);
+            // for chinese edges, we actually have to know if the opposite edge was allowed or not
+            opp_allowed =
+                opp_allowed || (opp_id.Is_Valid() ? costing->Allowed(opp_edge, opp_tile) : false);
+          }
+          if (predicate) {
+            break;
+          }
+        }
+        if (predicate) {
+          result_edge_ids.emplace(edge_allowed ? edge_id : vb::GraphId());
+          result_edge_ids.emplace(opp_allowed ? opp_id : vb::GraphId());
+        }
+      }
+    }
+  }
+
+// log the GeoJSON of avoided edges
+#ifdef LOGGING_LEVEL_TRACE
+  if (strategy == SearchStrategy::AVOID) {
+    LOG_TRACE("Avoided edges GeoJSON: \n" + to_geojson(result_edge_ids, reader));
+  }
+#endif
+  return result_edge_ids;
+}
+
+} // namespace loki
+} // namespace valhalla
---
+++ src/thor/matrix_action.cc
@@ -0,0 +1,125 @@
+#include "sif/autocost.h"
+#include "sif/bicyclecost.h"
+#include "sif/pedestriancost.h"
+#include "thor/costmatrix.h"
+#include "thor/timedistancebssmatrix.h"
+#include "thor/timedistancematrix.h"
+#include "thor/worker.h"
+#include "tyr/serializers.h"
+
+using namespace valhalla;
+using namespace valhalla::tyr;
+using namespace valhalla::midgard;
+using namespace valhalla::baldr;
+using namespace valhalla::sif;
+using namespace valhalla::thor;
+
+namespace {
+
+// return true if any location had a time set
+// also disable time if it doesn't make sense computationally
+bool has_time(Api& request) {
+  auto& options = request.options();
+  bool less_sources = options.sources().size() <= options.targets().size();
+  bool had_valid_time = false;
+  for (const auto& loc : options.sources()) {
+    if (!loc.date_time().empty()) {
+      if (!less_sources) {
+        add_warning(request, 201);
+        return false;
+      }
+      had_valid_time = true;
+      break;
+    }
+  }
+  for (const auto& loc : options.targets()) {
+    if (!loc.date_time().empty()) {
+      if (less_sources) {
+        add_warning(request, 202);
+        return false;
+      }
+      had_valid_time = true;
+      break;
+    }
+  }
+
+  return had_valid_time;
+}
+} // namespace
+
+namespace valhalla {
+namespace thor {
+
+constexpr uint32_t kCostMatrixThreshold = 5;
+
+std::string thor_worker_t::matrix(Api& request) {
+  // time this whole method and save that statistic
+  auto _ = measure_scope_time(request);
+
+  auto& options = *request.mutable_options();
+  adjust_scores(options);
+  auto costing = parse_costing(request);
+
+  // Distance scaling (miles or km)
+  double distance_scale = (options.units() == Options::miles) ? kMilePerMeter : kKmPerMeter;
+
+  // lambdas to do the real work
+  std::vector<TimeDistance> time_distances;
+  auto costmatrix = [&]() {
+    return costmatrix_.SourceToTarget(options.sources(), options.targets(), *reader, mode_costing,
+                                      mode, max_matrix_distance.find(costing)->second);
+  };
+  auto timedistancematrix = [&]() {
+    return time_distance_matrix_.SourceToTarget(*options.mutable_sources(),
+                                                *options.mutable_targets(), *reader, mode_costing,
+                                                mode, max_matrix_distance.find(costing)->second,
+                                                options.matrix_locations(),
+                                                options.date_time_type() == Options::invariant);
+  };
+
+  if (costing == "bikeshare") {
+    time_distances =
+        time_distance_bss_matrix_.SourceToTarget(options.sources(), options.targets(), *reader,
+                                                 mode_costing, mode,
+                                                 max_matrix_distance.find(costing)->second,
+                                                 options.matrix_locations());
+    return tyr::serializeMatrix(request, time_distances, distance_scale);
+  }
+  switch (source_to_target_algorithm) {
+    case SELECT_OPTIMAL:
+      // TODO - Do further performance testing to pick the best algorithm for the job
+      switch (mode) {
+        case travel_mode_t::kPedestrian:
+        case travel_mode_t::kBicycle:
+          // Use CostMatrix if number of sources and number of targets
+          // exceeds some threshold
+          if (options.sources().size() > kCostMatrixThreshold &&
+              options.targets().size() > kCostMatrixThreshold) {
+            time_distances = costmatrix();
+          } else {
+            time_distances = timedistancematrix();
+          }
+          break;
+        case travel_mode_t::kPublicTransit:
+          time_distances = timedistancematrix();
+          break;
+        default:
+          // force timedistance if traffic is desired and allowed
+          if (has_time(request)) {
+            time_distances = timedistancematrix();
+          } else {
+            time_distances = costmatrix();
+          }
+      }
+      break;
+    case COST_MATRIX:
+      time_distances = costmatrix();
+      break;
+    case TIME_DISTANCE_MATRIX:
+      time_distances = timedistancematrix();
+      break;
+  }
+  return tyr::serializeMatrix(request, time_distances, distance_scale);
+}
+} // namespace thor
+} // namespace valhalla
